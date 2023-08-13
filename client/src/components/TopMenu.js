e4b9da124485e022cb681b3f35dbdd1082df2859
olcayhank <kolcayhan1@gmail.com>
2023-08-13 14:35:44 +0300

    top menu improvements

--- client/src/components/TopMenu.js
+++ client/src/components/TopMenu.js
@@ -1,4 +1,9 @@
 import React, { useState } from "react";
+import { Spinner } from "react-bootstrap";
+import { AiOutlineUserAdd } from "react-icons/ai";
+import { RiBillFill } from "react-icons/ri";
+import { MdAnnouncement } from "react-icons/md";
+
 import Announcement from "./modals/Announcement";
 import AddStudent from "./modals/AddStudent";
 import { useAnnounceContext } from "../contexts/AnnounceContext";
@@ -6,18 +11,21 @@ import { useBillsContext } from "../contexts/BillContext";
 import { useStudentsContext } from "../contexts/StudentContext";
 import Card from "./Card";
 
-import { AiOutlineUserAdd } from "react-icons/ai";
-import { RiBillFill } from "react-icons/ri";
-import { MdAnnouncement } from "react-icons/md";
-import { Spinner } from "react-bootstrap";
-
 export default function TopMenu() {
-  const [isAddStudent, setIsAddStudent] = useState();
-  const [isAnnounce, setIsAnnounce] = useState();
-
   const { announces } = useAnnounceContext();
   const { bills } = useBillsContext();
   const { students } = useStudentsContext();
+
+  const [isAddStudent, setIsAddStudent] = useState(false);
+  const [isAnnounce, setIsAnnounce] = useState(false);
+
+  const openAddStudentModal = () => {
+    setIsAddStudent(true);
+  };
+
+  const openAnnounceModal = () => {
+    setIsAnnounce(true);
+  };
 
   return (
     <div className="d-flex flex-row flex-wrap justify-content-center align-items-center gap-5">
@@ -25,9 +33,7 @@ export default function TopMenu() {
         title="Student"
         length={students ? students.length : <Spinner />}
         color="#0B666A"
-        callback={() => {
-          setIsAddStudent(true);
-        }}
+        callback={openAddStudentModal}
         icon={<AiOutlineUserAdd color="white" size={50} />}
       />
 
@@ -43,9 +49,7 @@ export default function TopMenu() {
         title="Announces"
         length={announces ? announces.length : <Spinner />}
         color="#898121"
-        callback={() => {
-          setIsAnnounce(true);
-        }}
+        callback={openAnnounceModal}
         icon={<MdAnnouncement color="white" size={50} />}
       />
 
